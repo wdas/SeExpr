2497d85f584ee833dfe419d3b7eff5bd67faa6aa
Andrew Selle <aselle@andyselle.com>
2015-06-03 08:59:50 -0700

    Fix all the headers

--- src/demos/imageSynth2/examples2/TestExpressionsInPaint3D.sh
+++ src/demos/imageSynth2/examples2/TestExpressionsInPaint3D.sh
@@ -3,6 +3,21 @@
 #  Usage: ./TestExpressionsInPaint3D.sh <expression file root dir> <output dir>
 #  Output: one file called "result". One directory hierarchy same as <expression file root dir>
 #
+# Copyright Disney Enterprises, Inc.  All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License
+# and the following modification to it: Section 6 Trademarks.
+# deleted and replaced with:
+# 
+# 6. Trademarks. This License does not grant permission to use the
+# trade names, trademarks, service marks, or product names of the
+# Licensor and its affiliates, except as required for reproducing
+# the content of the NOTICE file.
+# 
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
 
 if [ "$#" -ne 2 ]; then
     echo "first arg is sefile root dir. second arg is output dir."
