1dba5888b8d0268c0b86490a6f7910d76f1b105b
Janet Berlin <janet.berlin@disneyanimation.com>
2016-02-02 16:12:08 -0800

    python script to generate test file for image examples

--- src/tests/genImageTestFile.py
+++ src/tests/genImageTestFile.py
@@ -23,16 +23,19 @@ outfile = sys.argv[2]
 
 # open outfile
 f = open(outfile, 'w')
-print >> f, "#include \"imageTests.h\"\n"
+print >> f, "#include <gtest.h>\n"
+print >> f, "extern void evalExpressionFile(const char *filepath);\n"
 
 for dir_name, sub_dirs, se_files in os.walk(rootdir):
     for se_file in se_files:
         fullpath = os.path.join(dir_name, se_file)
+        parent_dir = os.path.basename(dir_name).lstrip('.')
         (filename,ext) = os.path.splitext(se_file)
-        if(re.match('\.se', ext)):
-            print >> f, "TEST(" + os.path.basename(dir_name) + ", " + filename + ")"
+        filename = filename.replace(" ", "_").replace("&", "_").replace('.', '_')
+        if(re.match('\.se$', ext)):
+            print >> f, "TEST(" + parent_dir + ", " + filename + ")"
             print >> f, "{"
-            print >> f, "    evalExpressionFile2(\"" + fullpath + "\");"
+            print >> f, "    evalExpressionFile(\"" + fullpath + "\");"
             print >> f, "}\n"
 
 f.close()
