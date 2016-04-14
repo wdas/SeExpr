3b1de0ee234cc86e0c3ef2a16e408045b55f9136
Andrew Selle <andrew.selle@disney.com>
2016-04-13 20:00:59 -0700

    new examples to test basic evaluation
    Remove deprecated VarRef Codes
    
    Dump debugging data if we fail
    
    First implementation of a DataBlock
    
    The idea is the DataBlockCreator knows how to make typed objects.
    
    Multiple expressions can use the same DataBlock.
    You can create as many DataBlocks as you want (i.e. for each thread)
    Implemented on the interpreter (mostly in interpreter.cpp) and LLVM
    (ExprLLVMCodeGeneration.cpp).
    
    Example implemented in imageTests.cpp
    
    Fix block bug

--- src/SeExpr/Expression.h
+++ src/SeExpr/Expression.h
@@ -1,16 +1,16 @@
 /*
  Copyright Disney Enterprises, Inc.  All rights reserved.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License
  and the following modification to it: Section 6 Trademarks.
  deleted and replaced with:
- 
+
  6. Trademarks. This License does not grant permission to use the
  trade names, trademarks, service marks, or product names of the
  Licensor and its affiliates, except as required for reproducing
  the content of the NOTICE file.
- 
+
  You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
 */
@@ -28,10 +28,9 @@
 #include "Context.h"
 #include "ExprEnv.h"
 
-
 namespace llvm {
-    class ExecutionEngine;
-    class LLVMContext;
+class ExecutionEngine;
+class LLVMContext;
 }
 
 namespace SeExpr2 {
@@ -43,123 +42,52 @@ class Expression;
 class Interpreter;
 
 //! abstract class for implementing variable references
-class ExprVarRef
-{
-    ExprVarRef()
-        : _type(ExprType().Error().Varying())
-    {};
+class ExprVarRef {
+    ExprVarRef() : _type(ExprType().Error().Varying()) {};
 
- public:
-    ExprVarRef(const ExprType & type)
-        : _type(type)
-    {};
+  public:
+    ExprVarRef(const ExprType& type) : _type(type) {};
 
     virtual ~ExprVarRef() {}
 
     //! sets (current) type to given type
-    virtual void setType(const ExprType & type) { _type = type; };
+    virtual void setType(const ExprType& type) {
+        _type = type;
+    };
 
     //! returns (current) type
-    virtual ExprType type() const { return _type; };
+    virtual ExprType type() const {
+        return _type;
+    };
 
     //! returns this variable's value by setting result
-    virtual void eval(double* result)=0;
-    virtual void eval(const char** resultStr)=0;
+    virtual void eval(double* result) = 0;
+    virtual void eval(const char** resultStr) = 0;
 
-private:
+  private:
     ExprType _type;
 };
 
-// TODO: deprecate class and just use ExprVarRef base class
-/// simple vector variable reference reference base class
-// class ExprVectorVarRef : public ExprVarRef
-// {
-//  public:
-//     ExprVectorVarRef(int dim = 3)
-//         : ExprVarRef(ExprType().FP(dim).Varying())
-//     {};
-
-//     virtual bool isVec() { return 1; }
-//     virtual void eval(const ExprVarNode* node, Vec3d& result)=0;
-//     virtual void eval(double* result){
-//         Vec3d ret;
-//         eval(0,ret);
-//         for(int k=0;k<3;k++) result[k]=ret[k];
-//     }
-//     virtual void eval(const char** result){assert(false);}
-// };
-
-
-// TODO: deprecate class and just use ExprVarRef base class
-/// simple scalar variable reference reference base class
-// class ExprScalarVarRef : public ExprVarRef
-// {
-//  public:
-//     ExprScalarVarRef()
-//         : ExprVarRef(ExprType().FP(1).Varying())
-//     {};
-
-//     virtual bool isVec() { return 0; }
-//     virtual void eval(const ExprVarNode* node, Vec3d& result)=0;
-//     virtual void eval(double* result){
-//         Vec3d ret;
-//         eval(0,ret);
-//         for(int k=0;k<1;k++) result[k]=ret[k];
-//     }
-//     virtual void eval(const char** result){assert(false);}
-
-// };
-
-#if 0
-/// uses internally to represent local variables
-class ExprLocalVarRef : public ExprVarRef
-{
-    ExprLocalVarRef()
-        : ExprVarRef(ExprType().Error().Varying()), val(0)
-    {}
-
- public:
-    union{
-        double *val;
-        const char* s;
-    };
-    ExprLocalVarRef(const ExprType & intype)
-        :ExprVarRef(intype),val(0)
-    {
-        if(type().isFP()) val=new double[type().dim()];
-    }
-
-    virtual ~ExprLocalVarRef()
-    {delete [] val;}
-
-    virtual void evaluate(const ExprVarNode* node,const ExprEvalResult& evalResult){
-        if(type().isString()) *evalResult.str=s;
-        else{
-            int d=type().dim();
-            for(int k=0;k<d;k++) evalResult.fp[k]=val[k];
-        }
-    }
-};
-#endif
-
 class LLVMEvaluator;
+class ExprVarBlock;
 
 /// main expression class
-class Expression
-{
- public:
+class Expression {
+  public:
     //! Types of evaluation strategies that are available
-    enum EvaluationStrategy {UseInterpreter, UseLLVM};
+    enum EvaluationStrategy {
+        UseInterpreter,
+        UseLLVM
+    };
     //! What evaluation strategy to use by default
     static EvaluationStrategy defaultEvaluationStrategy;
     //! Whether to debug expressions
     static bool debugging;
 
-    //typedef std::map<std::string, ExprLocalVarRef> LocalVarTable;
+    // typedef std::map<std::string, ExprLocalVarRef> LocalVarTable;
 
     //! Represents a parse or type checking error in an expression
-    struct Error
-    {
+    struct Error {
         //! Text of error
         std::string error;
 
@@ -169,21 +97,23 @@ class Expression
         //! Error end offset in expression string
         int endPos;
 
-        Error(const std::string& errorIn,const int startPosIn,const int endPosIn)
-            :error(errorIn),startPos(startPosIn),endPos(endPosIn)
-        {}
+        Error(const std::string& errorIn, const int startPosIn, const int endPosIn)
+            : error(errorIn), startPos(startPosIn), endPos(endPosIn) {}
     };
 
     Expression(EvaluationStrategy be = Expression::defaultEvaluationStrategy);
-    Expression( const std::string &e, const ExprType & type = ExprType().FP(3), EvaluationStrategy be = Expression::defaultEvaluationStrategy, const Context& context=Context::global());
+    Expression(const std::string& e,
+               const ExprType& type = ExprType().FP(3),
+               EvaluationStrategy be = Expression::defaultEvaluationStrategy,
+               const Context& context = Context::global());
 
     virtual ~Expression();
 
     /** Sets desired return value.
         This will allow the evaluation to potentially be optimized. */
-    void setDesiredReturnType(const ExprType & type);
+    void setDesiredReturnType(const ExprType& type);
 
-    /** Set expression string to e.  
+    /** Set expression string to e.
         This invalidates all parsed state. */
     void setExpr(const std::string& e);
 
@@ -191,98 +121,95 @@ class Expression
     const std::string& getExpr() const { return _expression; }
 
     /** Check expression syntax.  Expr will be parsed if needed.  If
-	this returns false, the error message can be accessed via
-	parseError() */
+        this returns false, the error message can be accessed via
+        parseError() */
     bool syntaxOK() const;
 
     /** Check if expression is valid.  Expr will be parsed if
     needed. Variables and functions will also be bound.  If this
     returns false, the error message can be accessed via
     parseError() */
-    bool isValid() const { prepIfNeeded(); return _isValid; }
+    bool isValid() const {
+        prepIfNeeded();
+        return _isValid;
+    }
 
     /** Get parse error (if any).  First call syntaxOK or isValid
-	to parse (and optionally bind) the expression. */
+        to parse (and optionally bind) the expression. */
     const std::string& parseError() const { return _parseError; }
 
     /** Get a reference to a list of parse errors in the expression.
         The error structure gives location information as well as the errors itself. */
-    const std::vector<Error>& getErrors() const
-    {return _errors;}
+    const std::vector<Error>& getErrors() const { return _errors; }
 
     /** Get a reference to a list of the ranges where comments occurred */
-    const std::vector<std::pair<int,int> >& getComments() const
-    {return _comments;}
+    const std::vector<std::pair<int, int> >& getComments() const { return _comments; }
 
     /** Check if expression is constant.
-	Expr will be parsed if needed.  No binding is required. */
+        Expr will be parsed if needed.  No binding is required. */
     bool isConstant() const;
 
-    /** Determine whether expression uses a particular external variable. 
-	Expr will be parsed if needed.  No binding is required. */
+    /** Determine whether expression uses a particular external variable.
+        Expr will be parsed if needed.  No binding is required. */
     bool usesVar(const std::string& name) const;
 
     /** Determine whether expression uses a particular function.
-	Expr will be parsed if needed.  No binding is required. */
+        Expr will be parsed if needed.  No binding is required. */
     bool usesFunc(const std::string& name) const;
 
     /** Returns whether the expression contains and calls to non-threadsafe */
-    bool isThreadSafe() const
-    {return _threadUnsafeFunctionCalls.size()==0;}
+    bool isThreadSafe() const { return _threadUnsafeFunctionCalls.size() == 0; }
 
     /** Internal function where parse tree nodes can register violations in
         thread safety with the main class. */
-    void setThreadUnsafe(const std::string& functionName) const
-    {_threadUnsafeFunctionCalls.push_back(functionName);}
+    void setThreadUnsafe(const std::string& functionName) const { _threadUnsafeFunctionCalls.push_back(functionName); }
 
     /** Returns a list of functions that are not threadSafe **/
-    const std::vector<std::string>& getThreadUnsafeFunctionCalls() const
-    {return _threadUnsafeFunctionCalls;}
-    
+    const std::vector<std::string>& getThreadUnsafeFunctionCalls() const { return _threadUnsafeFunctionCalls; }
+
     /** Get wantVec setting */
     bool wantVec() const { return _wantVec; }
 
     /** Determine if expression computes a vector (may be false even
-	if wantVec is true).  Expr will be parsed and variables and
-	functions will be bound if needed. */
+        if wantVec is true).  Expr will be parsed and variables and
+        functions will be bound if needed. */
     bool isVec() const;
 
     /** Return the return type of the expression.  Currently may not
         match the type set in setReturnType.  Expr will be parsed and
         variables and functions will be bound if needed. */
-    const ExprType & returnType() const;
+    const ExprType& returnType() const;
 
     // TODO: make this deprecated
     /** Evaluates and returns float (check returnType()!) */
-    const double* evalFP() const;
+    const double* evalFP(ExprVarBlock* varBlock = nullptr) const;
 
     // TODO: make this deprecated
     /** Evaluates and returns string (check returnType()!) */
-    const char* evalStr() const;
+    const char* evalStr(ExprVarBlock* varBlock = nullptr) const;
 
     /** Reset expr - force reparse/rebind */
     void reset();
 
     /** override resolveVar to add external variables */
-    virtual ExprVarRef* resolveVar(const std::string& name) const {return 0;}
+    virtual ExprVarRef* resolveVar(const std::string& name) const { return 0; }
 
     /** override resolveFunc to add external functions */
-    virtual ExprFunc* resolveFunc(const std::string& name) const {return 0;}
+    virtual ExprFunc* resolveFunc(const std::string& name) const { return 0; }
 
     /** records an error in prep or parse stage */
-    void addError(const std::string& error,const int startPos,const int endPos) const
-
-    {_errors.push_back(Error(error,startPos,endPos));}
+    void addError(const std::string& error, const int startPos, const int endPos) const {
+        _errors.push_back(Error(error, startPos, endPos));
+    }
 
     /** records a comment */
-    void addComment(int pos,int length)
-    {_comments.push_back(std::pair<int,int>(pos,length+pos-1));}
+    void addComment(int pos, int length) { _comments.push_back(std::pair<int, int>(pos, length + pos - 1)); }
 
     /** Returns a read only map of local variables that were set **/
-    //const LocalVarTable& getLocalVars() const {return _localVars;}
+    // const LocalVarTable& getLocalVars() const {return _localVars;}
 
     /** An immutable reference to access context parameters from say ExprFuncX's */
-    const Context& context() const {return *_context;}
+    const Context& context() const { return *_context; }
     void setContext(const Context& context);
 
     /** Debug printout of parse tree */
@@ -294,16 +221,18 @@ class Expression
     /** Debug printout of LLVM evaluation  **/
     void debugPrintLLVM() const;
 
- private:
+  private:
     /** No definition by design. */
-    Expression( const Expression &e );
-    Expression &operator=( const Expression &e );
+    Expression(const Expression& e);
+    Expression& operator=(const Expression& e);
 
     /** Parse, and remember parse error if any */
     void parse() const;
 
     /** Parse, but only if not yet parsed */
-    void parseIfNeeded() const { if (!_parsed) parse(); }
+    void parseIfNeeded() const {
+        if (!_parsed) parse();
+    }
 
     /** Prepare expression (bind vars/functions, etc.)
     and remember error if any */
@@ -323,25 +252,26 @@ class Expression
     /** Context for out of band function parameters */
     const Context* _context;
 
- protected:
-
+  protected:
     /** Computed return type. */
     mutable ExprType _desiredReturnType;
 
     /** Variable environment */
     mutable ExprVarEnv* _varEnv;
     /** Parse tree (null if syntax is bad). */
-    mutable ExprNode *_parseTree;
+    mutable ExprNode* _parseTree;
 
     /** Prepare, but only if not yet prepped */
-    void prepIfNeeded() const { if (!_prepped) prep(); }
+    void prepIfNeeded() const {
+        if (!_prepped) prep();
+    }
 
- private:
+  private:
     /** Flag if we are valid or not */
     mutable bool _isValid;
     /** Flag set once expr is parsed/prepped (parsing is automatic and lazy) */
     mutable bool _parsed, _prepped;
-    
+
     /** Cached parse error (returned by isValid) */
     mutable std::string _parseError;
 
@@ -349,7 +279,7 @@ class Expression
     mutable std::vector<Error> _errors;
 
     /** Cached comments */
-    mutable std::vector<std::pair<int,int> > _comments;
+    mutable std::vector<std::pair<int, int> > _comments;
 
     /** Variables used in this expr */
     mutable std::set<std::string> _vars;
@@ -358,7 +288,7 @@ class Expression
     mutable std::set<std::string> _funcs;
 
     /** Local variable table */
-    //mutable LocalVarTable _localVars;
+    // mutable LocalVarTable _localVars;
 
     /** Whether or not we have unsafe functions */
     mutable std::vector<std::string> _threadUnsafeFunctionCalls;
@@ -379,7 +309,7 @@ class Expression
     void addFunc(const char* n) const { _funcs.insert(n); }
 
     ////! get local variable reference (this is for internal use)
-    //ExprVarRef* resolveLocalVar(const char* n) const {
+    // ExprVarRef* resolveLocalVar(const char* n) const {
     //    LocalVarTable::iterator iter = _localVars.find(n);
     //    if (iter != _localVars.end()) return &iter->second;
     //    return 0;
@@ -391,7 +321,6 @@ class Expression
     /*     return &_localVars[n];  */
     /* } */
 };
-
 }
 
 #endif
--- src/SeExprEditor/ControlSpec.h
+++ src/SeExprEditor/ControlSpec.h
@@ -1,16 +1,16 @@
 /*
  Copyright Disney Enterprises, Inc.  All rights reserved.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License
  and the following modification to it: Section 6 Trademarks.
  deleted and replaced with:
- 
+
  6. Trademarks. This License does not grant permission to use the
  trade names, trademarks, service marks, or product names of the
  Licensor and its affiliates, except as required for reproducing
  the content of the NOTICE file.
- 
+
  You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
 */
@@ -33,15 +33,14 @@ namespace SeExpr2 {
 
 //! Generic Expression control specification
 class ControlSpec {
- public:
-    ControlSpec(const ExprNode& node)
-        :_start(node.startPos()), _end(node.endPos())
-    {};
-    virtual ~ControlSpec(){};
+  public:
+    ControlSpec(const ExprNode& node) : _start(node.startPos()), _end(node.endPos()) {};
+    virtual ~ControlSpec() {};
 
     //! Generates a replacement string based on changes to the spec
     virtual std::string toString() const = 0;
- protected:
+
+  protected:
     //! Name of control
     std::string _name;
     //! Start position of text in original source
@@ -52,44 +51,48 @@ class ControlSpec {
 
 //! Variable equals scalar control specification
 class ExprScalarAssignSpec : public ControlSpec {
- public:
+  public:
     ExprScalarAssignSpec(const ExprAssignNode& node);
     virtual std::string toString() const;
-    inline double value() const { return _val; };
+    inline double value() const {
+        return _val;
+    };
     static const ExprScalarAssignSpec* match(const ExprNode* node);
 
- private:
+  private:
     //! Range of values
-    double _min,_max;
+    double _min, _max;
     //! Current Value
     double _val;
 };
 
 //! Variable equals vector control specification
 class ExprVectorAssignSpec : public ControlSpec {
- public:
+  public:
     ExprVectorAssignSpec(const ExprAssignNode& node);
     virtual std::string toString() const;
-    inline const Vec3d & value() const { return _val; };
+    inline const Vec3d& value() const {
+        return _val;
+    };
     static const ExprVectorAssignSpec* match(const ExprNode* node);
 
- private:
+  private:
     //! Range of values
-    double _min,_max;
+    double _min, _max;
     //! Current Value
     Vec3d _val;
 };
 
 //! Curve assignment expression. Assignment of curve to a variable.
-template<class T>
+template <class T>
 class ExprCurveAssignSpec : public ControlSpec {
 
- public:
+  public:
     ExprCurveAssignSpec(const ExprAssignNode& node);
     virtual std::string toString() const;
     static const ExprCurveAssignSpec* match(const ExprNode* node);
 
- private:
+  private:
     //! Lookup subexpression text
     std::string _lookupText;
     //! Control points of curve spline
@@ -97,41 +100,47 @@ class ExprCurveAssignSpec : public ControlSpec {
 };
 
 class ExprStrSpec : public ControlSpec {
-    enum Type {STRING,FILE,DIRECTORY};
- public:
+    enum Type {
+        STRING,
+        FILE,
+        DIRECTORY
+    };
+
+  public:
     //! Takes name and type comments and takes ownership of them!
-    ExprStrSpec(const ExprStrNode& node,char* name,Type type)
-        : ControlSpec(node),_str(node.str()),_type(type)
-    {
-        _name=name;
+    ExprStrSpec(const ExprStrNode& node, char* name, Type type) : ControlSpec(node), _str(node.str()), _type(type) {
+        _name = name;
     }
 
     virtual std::string toString() const;
     static const ExprStrSpec* match(const ExprNode* node);
-private:
+
+  private:
     std::string _str;
     Type _type;
 };
 
-
-
 /// Examiner that builds a list of specs potentially used in widgets (for qdgui)
 class SpecExaminer : public Examiner<true> {
 
- public:
+  public:
     ~SpecExaminer();
 
     virtual bool examine(const ExprNode* examinee);
-    virtual void reset() { _specList.clear(); };
-    inline int length() const { return _specList.size(); };
-    inline const ControlSpec* spec(int i) const { return _specList[i]; };
-    inline std::vector<const ControlSpec*>::const_iterator       begin() const;
-    inline std::vector<const ControlSpec*>::const_iterator const end  () const;
+    virtual void reset() {
+        _specList.clear();
+    };
+    inline int length() const {
+        return _specList.size();
+    };
+    inline const ControlSpec* spec(int i) const {
+        return _specList[i];
+    };
+    inline std::vector<const ControlSpec*>::const_iterator begin() const;
+    inline std::vector<const ControlSpec*>::const_iterator const end() const;
 
- private:
+  private:
     std::vector<const ControlSpec*> _specList;
-
 };
-
 }
 #endif
