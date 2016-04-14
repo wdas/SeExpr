fc4ce3bf3816fd44bcf890aee5f4bfe57f207310
Andrew Selle <andrew.selle@disney.com>
2016-04-14 15:42:18 -0700

    Add missing licenses

--- src/tests/imageTestsReport.py
+++ src/tests/imageTestsReport.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python
-# generate timing report for image tests against 3 versions of seexpr
-# imageTestsReport.py 2> /tmp/imageTests.csv
+#!/bin/env python
+# Copyright Disney Enterprises, Inc.  All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License
+# and the following modification to it: Section 6 Trademarks.
+# deleted and replaced with:
+# 
+# 6. Trademarks. This License does not grant permission to use the
+# trade names, trademarks, service marks, or product names of the
+# Licensor and its affiliates, except as required for reproducing
+# the content of the NOTICE file.
+# 
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
 
 import sys
 import os
