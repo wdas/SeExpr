19854f8985f6aa376b35cd84b84f2521778fef02
Brent Burley <brent.burley@disneyanimation.com>
2019-05-07 12:49:24 -0700

    Fixes SEEXPR-206: create widgets for string args/variables

--- src/ui/Editable.h
+++ src/ui/Editable.h
@@ -162,20 +162,32 @@ struct VectorEditable : public Editable {
 struct StringEditable : public Editable {
     std::string v;
     std::string type;
-    StringEditable(int startPos, int endPos, const std::string& val) : Editable("unknown", startPos, endPos), v(val)
+    StringEditable(const std::string& name, int startPos, int endPos, const std::string& val) : Editable(name, startPos, endPos), v(val)
     {
     }
 
     bool parseComment(const std::string& comment)
     {
-        char namebuf[1024], typebuf[1024];
-        int parsed = sscanf(comment.c_str(), "#%s %s", typebuf, namebuf);
-        if (parsed == 2) {
-            name = namebuf;
-            type = typebuf;
-            return true;
+        if (name == "") {
+            char namebuf[1024], typebuf[1024];
+            int parsed = sscanf(comment.c_str(), "#%s %s", typebuf, namebuf);
+            if (parsed == 2) {
+                name = namebuf;
+                type = typebuf;
+                return true;
+            } else {
+                return false;
+            }
         } else {
-            return false;
+            char typebuf[1024];
+            int parsed = sscanf(comment.c_str(), "#%s", typebuf);
+            if (parsed == 1) {
+                type = typebuf;
+            }
+            else {
+                type = "string";
+            }
+            return true;
         }
     }
 
