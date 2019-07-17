e586d07524df14528bb842f4be9335e07257d3f2
David Aguilar <davvid@gmail.com>
2019-07-16 20:53:36 -0700

    py: support unicode strings under python2
    Add a Python2 compatibility wrapper around the Boost::Python generated
    AST class to allow construction using unicode strings.
    
    This aids python2 applications that use unicode literals or are
    internally unicode.  This constructor is only needed for Python2 --
    Boost::Python handles both str and bytes for std::string in Python3.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- src/py/__init__.py
+++ src/py/__init__.py
@@ -27,9 +27,30 @@ help(SeExprPy.utils)
 """
 
 from __future__ import absolute_import
+import sys
 
 from .core import AST as _AST, ASTHandle, ASTType
 from .utils import (
     getComment, Edits, traverseCallback, traverseFilter, printTree
 )
 
+_PY2 = sys.version_info[0] == 2
+try:
+    ustr = unicode
+except NameError:
+    ustr = str
+
+
+def _encode(obj, encoding='utf-8'):
+    # The Python2 Boost.Python API requires byte strings
+    if _PY2 and isinstance(obj, ustr):
+        value = obj.encode(encoding)
+    else:
+        value = obj
+    return value
+
+
+# Override AST to handle unicode vs. str constructor values
+class AST(_AST):
+    def __init__(self, string):
+        super(AST, self).__init__(_encode(string))
--- src/tests/basicPython.py
+++ src/tests/basicPython.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
+from __future__ import absolute_import, division, unicode_literals
 import sys
 import unittest
+
 try:
     import SeExprPy
 except ImportError:
     sys.stderr.write(
         'WARNING: SeExprPy is not importable: Python support is disabled\n')
     sys.exit(0)
+
+
+MAP_EXPR = """
+    # a cool expression
+    a = 3;
+    if (a > 5) {
+        b = 10;
+    } else if (a > 10) {
+        b = 20;
+    }
+    c += 10 + P;
+
+    # note: intentional use of different quotes
+    map("a.png") + map('b.png') + map("c.png")
+"""
 
 
 class SeExprTestCase(unittest.TestCase):
@@ -18,20 +35,7 @@ class SeExprTestCase(unittest.TestCase):
     def test_utils_namespace(self):
         self.assertTrue(hasattr(SeExprPy, 'utils'))
 
-    def test_AST(self):
-        expr = """
-        # a cool expression
-        a = 3;
-        if (a > 5) {
-            b = 10;
-        } else if (a > 10) {
-            b = 20;
-        }
-        c += 10 + P;
-
-        # note: intentional use of different quotes
-        map("a.png") + map('b.png') + map("c.png")
-        """
+    def test_AST(self, expr=MAP_EXPR):
         ast = SeExprPy.AST(expr)
         edits = SeExprPy.Edits(expr)
         maps = []
@@ -49,6 +53,9 @@ class SeExprTestCase(unittest.TestCase):
 
         expect_maps = ['a.png', 'b.png', 'c.png']
         self.assertEqual(expect_maps, maps)
+
+    def test_AST_bytes(self):
+        self.test_AST(expr=MAP_EXPR.encode('utf-8'))
 
 
 if __name__ == '__main__':
