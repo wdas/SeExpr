f1980acb3bbb0b32fd6aadd53269d0f301868acc
Damien Courtois <dcourtois@isotropix.com>
2019-10-03 16:56:20 +0200

    fixed dumb mistake in string un-escaping method + updated the unit test to check regressions

--- src/SeExpr2/StringUtils.h
+++ src/SeExpr2/StringUtils.h
@@ -26,22 +26,22 @@ inline std::string unescapeString(const std::string& string) {
     int index = 0;
     bool special = false;
     for (char c : string) {
-        if (c == '\\') {
-            special = true;
+        if (special == true) {
+            special = false;
+            switch (c) {
+                case 'n':   output[index++] = '\n'; break;
+                case 'r':   output[index++] = '\r'; break;
+                case 't':   output[index++] = '\t'; break;
+                case '\\':  output[index++] = '\\'; break;
+                case '"':   output[index++] = '\"'; break;
+                default:
+                    // leave the escape sequence as it was
+                    output[index++] = '\\';
+                    output[index++] = c;
+            }
         } else {
-            if (special == true) {
-                special = false;
-                switch (c) {
-                    case 'n':   output[index++] = '\n'; break;
-                    case 'r':   output[index++] = '\r'; break;
-                    case 't':   output[index++] = '\t'; break;
-                    case '\\':  output[index++] = '\\'; break;
-                    case '"':   output[index++] = '\"'; break;
-                    default:
-                        // leave the escape sequence as it was
-                        output[index++] = '\\';
-                        output[index++] = c;
-                }
+            if (c == '\\') {
+                special = true;
             } else {
                 output[index++] = c;
             }
--- src/tests/string.cpp
+++ src/tests/string.cpp
@@ -100,11 +100,11 @@ TEST(StringTests, Constant) {
     EXPECT_STREQ(expr.evalStr(), "hello world !");
 
     // check that strings are correctly unescaped
-    StringExpression expr7("\"hello\\t\\n\\\"world\\\"\"");
+    StringExpression expr7("\"hello\\\\\\t\\n\\\"world\\\"\"");
     EXPECT_TRUE(expr7.isValid() == true);
     EXPECT_TRUE(expr7.returnType().isString() == true);
     EXPECT_TRUE(expr7.isConstant() == true);
-    EXPECT_STREQ(expr7.evalStr(), "hello\t\n\"world\"");
+    EXPECT_STREQ(expr7.evalStr(), "hello\\\t\n\"world\"");
 }
 
 TEST(StringTests, Variable) {
